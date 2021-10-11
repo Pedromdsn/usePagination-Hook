7e059e1f147e5a78dd003a44ad80776793acc096
pedromdsn <pedromdsn.2002@hotmail.com>
2021-10-11 19:39:57 +0100

    fit: Style and ico

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,9 @@ import React from "react"
 import ReactDOM from "react-dom"
 import App from "./pages"
 
-import "./style/index.css"
 import { Contexts } from "./context"
+
+import "./style/index.css"
 
 ReactDOM.render(
 	<React.StrictMode>
